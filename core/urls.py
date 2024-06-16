54c5ed361b03186f65930107508035a2d3b8be59
Abolarin <abolarin.david@lmu.edu.ng>
2024-06-16 11:38:28 +0100

    Updated proceed

--- core/urls.py
+++ core/urls.py
@@ -9,13 +9,14 @@ urlpatterns = [
     path("create-account/", CreateAccount, name="create-account"),
     path("signin/", Signin, name="signin"),
     path("signout/", Signout,name="signout"),
-    path("get-objquestion/<str:code>", GetObJQuestions, name="get-objquestion"),
-    path("get-theoryquestion/<str:code>", GetTheoryQuestions, name="get-theoryquestion"),
-    path("start-exam", StartExam, name="start-exam"),
-    path("answer-objquestion/<str:pk>", AnswerObJQuestion, name="answer-objquestion"),
-    path("answer-theoryquestion/<str:pk>", AnswerTheoryQuestion, name="answer-theoryquestion"),
+    path("get-objquestion/<str:code>/", GetObJQuestions, name="get-objquestion"),
+    path("get-theoryquestion/<str:code>/", GetTheoryQuestions, name="get-theoryquestion"),
+    path("start-exam/", StartExam, name="start-exam"),
+    path("answer-objquestion/<str:pk>/", AnswerObJQuestion, name="answer-objquestion"),
+    path("answer-theoryquestion/<str:pk>/", AnswerTheoryQuestion, name="answer-theoryquestion"),
     path("submit-exam/<str:exam_id>/", submit_exam, name="submit-exam"),
     path("submit-answer-exam/<str:pk>/", submit_answer_exam, name="submit_answer_exam"),
+    path("proceed-exam/<str:code>/", ProceedExam, name="proceed-exam")
 
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,30 +1,31 @@
 from rest_framework.generics import CreateAPIView
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user_model
 from rest_framework.response import Response
 from rest_framework import status
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
 from rest_framework.decorators import api_view
 from .models import QuestionModel, Exam, Profile, TheoryQuestion
 from .serializers import SerializerQuestion, SerializerExam, SerializerCreateAccount, SerializerTheory
 from django.contrib.auth.decorators import login_required
 from django.core.mail import send_mail
 from django.http import JsonResponse
+from random import shuffle
+
+User = get_user_model()
 
 class CheckView(CreateAPIView):
-    questions = QuestionModel.objects.all()
+    queryset = QuestionModel.objects.all()
     serializer_class = SerializerQuestion
 
 @login_required
-@api_view(['POST'])  
+@api_view(['POST'])
 def CreateQuestion(request, exam_id):
-    print(exam_id)
     try:
         exam = Exam.objects.get(id=exam_id)
     except Exam.DoesNotExist:
         return Response({"detail": "Exam not found."}, status=status.HTTP_404_NOT_FOUND)
 
     if request.method == "POST":
-        print("Hiii") 
         serializer = SerializerQuestion(data=request.data)
         if serializer.is_valid():
             serializer.save(owner=exam)
@@ -32,58 +33,56 @@ def CreateQuestion(request, exam_id):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @login_required
+@api_view(['POST'])
 def CreateTheory(request, exam_id):
     try:
         exam = Exam.objects.get(id=exam_id)
     except Exam.DoesNotExist:
         return Response({"detail": "Exam not found."}, status=status.HTTP_404_NOT_FOUND)
+    
     if request.method == "POST":
         serializer = SerializerTheory(data=request.data)
         if serializer.is_valid():
+            serializer.save(owner=exam)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
 @login_required
-@api_view(['POST'])  
+@api_view(['POST'])
 def CreateExam(request):
     if request.method == "POST":
         profile = request.user.profile
         serializer = SerializerExam(data=request.data)
         if serializer.is_valid():
             exam = serializer.save(owner=profile)
-        # Store exam_id in session
             return Response({"examId": str(exam.id)}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
-
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @login_required
+@api_view(['POST'])
 def submit_exam(request, exam_id):
     exam = get_object_or_404(Exam, id=exam_id)
     user_email = exam.owner.email
     name = exam.owner.name
-    
-    # Generate a code
     code = exam.code
     
-    # Send the code to the user's email
-    subject = f"{name} You have Sucessfully created your exam questions!!"
+    subject = f"{name}, you have successfully created your exam questions!"
     message = f"The exam code for {exam.name} is {code}. Share this code with your students so they can take the exam."
-    sender_email = "phedave05@gmail.com"  # Your email address or the sender's email
+    sender_email = "phedave05@gmail.com"
     send_mail(subject, message, sender_email, [user_email])
+    
     return JsonResponse({"detail": "Exam submitted and code sent successfully"}, status=200)
 
-
-@api_view(['POST']) 
+@api_view(['POST'])
 def CreateAccount(request):
     if request.method == "POST":
         serializer = SerializerCreateAccount(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response({"detail": "User created successfully"}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)     
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['POST']) 
+@api_view(['POST'])
 def Signin(request):
     if request.method == 'POST':
         username = request.data.get("username")
@@ -92,20 +91,19 @@ def Signin(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
-            return Response({"Details: Login Succesfull"}, status=status.HTTP_200_OK)
+            return Response({"Details": "Login Successful"}, status=status.HTTP_200_OK)
         else:
-            return Response({"Details: Invalid Credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+            return Response({"Details": "Invalid Credentials"}, status=status.HTTP_401_UNAUTHORIZED)
 
-    return Response({"Details: Method not allowed"}, status=status.HTTP_405__METHOD_NOT_ALLOWED)
+    return Response({"Details": "Method not allowed"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
-
+@login_required
+@api_view(['POST'])
 def Signout(request):
-    logout(user)
+    logout(request)
     return redirect("signin")
 
-question_answered_obj = []
-
-api_view(['GET'])  
+@api_view(['GET'])
 def GetObJQuestions(request, code):
     exam = get_object_or_404(Exam, code=code)
     questionobj = exam.questionmodel_set.all()
@@ -124,17 +122,14 @@ def GetObJQuestions(request, code):
                 'option1': options[0],
                 'option2': options[1],
                 'option3': options[2],
-                'answer': options[3],  # Correct answer remains at the same index
+                'answer': options[3],
             }
             serializer = SerializerQuestion(shuffled_question)
             return Response(serializer.data, status=status.HTTP_200_OK)
     
     return redirect("get-theoryquestion", code=code)
 
-
-
-
-api_view(['GET'])  
+@api_view(['GET'])
 def GetTheoryQuestions(request, code):
     exam = get_object_or_404(Exam, code=code)
     questiontheory = exam.theoryquestion_set.all()
@@ -150,8 +145,6 @@ def GetTheoryQuestions(request, code):
     
     return redirect("submit_answer_exam", code=code)
 
-
-
 @api_view(['POST'])
 def StartExam(request):
     if request.method == "POST":
@@ -160,14 +153,16 @@ def StartExam(request):
         request.session['uniqueName'] = name_of_user
         request.session['score'] = 0
         request.session['theory_questions_answered'] = []
-        return Response({"code": code}, status=status.HTTP_200_OK)
+        return Response({"code": code, "userName": name_of_user}, status=status.HTTP_200_OK)
 
 
+@api_view(['POST'])
+def ProceedExam(request, code):
+    if request.method == "POST":
+        return redirect("get-objquestion", code=code)
 
 
-
-score = []
-api_view(['POST'])  
+@api_view(['POST'])
 def AnswerObJQuestion(request, pk):
     if request.method == "POST":
         obj_question = get_object_or_404(QuestionModel, id=pk)
@@ -177,10 +172,7 @@ def AnswerObJQuestion(request, pk):
             request.session['score'] += 1
         return redirect("get-objquestion", code=code)
 
-
-
-
-api_view(['POST'])  
+@api_view(['POST'])
 def AnswerTheoryQuestion(request, pk):
     if request.method == "POST":
         theory_question = get_object_or_404(TheoryQuestion, id=pk)
@@ -190,12 +182,11 @@ def AnswerTheoryQuestion(request, pk):
             theory_question.question: option_picked 
         }
         request.session['theory_questions_answered'].append(answer)
-        facilitator_email = theory_question.owner.ownwer.email
+        facilitator_email = theory_question.owner.owner.email
 
-    return redirect("get-theoryquestion", code=code)
+        return redirect("get-theoryquestion", code=code)
 
-
-api_view(['POST'])  
+@api_view(['POST'])
 def submit_answer_exam(request, code):
     score = request.session['score']
     exam = get_object_or_404(Exam, code=code)
@@ -203,14 +194,9 @@ def submit_answer_exam(request, code):
     uniqueName = request.session['uniqueName']
     theory_questions_answered = request.session['theory_questions_answered']
 
-    subject = f"{uniqueName} has Finished His/Her Exam!!"
-    message = f"The score is {score}, below and here are the theory questions {theory_questions_answered}"
-    sender_email = "phedave05@gmail.com"  # Your email address or the sender's email
+    subject = f"{uniqueName} has finished their exam!"
+    message = f"The score is {score}. Here are the theory questions and answers: {theory_questions_answered}"
+    sender_email = "phedave05@gmail.com"
     send_mail(subject, message, sender_email, [email])
-    return Response({"detail": "Exam submitted and code sent successfully"}, status=status.HTTP_200_OK)
-
-
-
     
-
-
+    return Response({"detail": "Exam submitted and code sent successfully"}, status=status.HTTP_200_OK)
